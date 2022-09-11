83fec94b12529fac436d942c52fd6df4ac84c833
Toskyll <jans.guillope@gmail.com>
2022-09-11 13:41:16 +0200

    updates

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 // Import the necessary discord.js classes
-const { Client, GatewayIntenteBits } = require("discord.js");
+const { Client, GatewayIntentBits } = require("discord.js");
+const dotenv = require("dotenv");
+dotenv.config();
+
+// Create client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// When client ready run code (only once)
+client.once("ready", () => {
+  console.log("Ready!");
+});
+
+// Login discord bot with secret token
+client.login(process.env.TOKEN);
